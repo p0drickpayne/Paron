ab5c5a5cf51e1290827d6504c409924b31d49888
jakob salomonsson <jakob@salomonssons.se>
2020-03-03 15:31:45 +0100

    Initial Commit

---
+++ Paron/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Paron
+//
+//  Created by Jakob Salomonsson on 2020-03-03.
+//  Copyright © 2020 Jakob Salomonsson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
